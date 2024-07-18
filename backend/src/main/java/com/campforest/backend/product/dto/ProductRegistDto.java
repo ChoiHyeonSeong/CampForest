8e49cc2412e0a7d01378afc01a536d68a0e893fd
이예찬 <dldpcks34@naver.com>
2024-07-18 17:08:37 +0900

    feat: 장비 대여/판매 게시글 등록 API 개발
    장비 관련 아키텍처 구현 및 게시글 등록 기능 개발
    (아직, 사용자와 연관관계 매핑은 안되었고, 카테고리에 대한 리팩토링 필요)
    
    Related to: #S11P12D208-68

---
+++ backend/src/main/java/com/campforest/backend/product/dto/ProductRegistDto.java
@@ -0,0 +1,42 @@
+package com.campforest.backend.product.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.campforest.backend.product.model.Category;
+import com.campforest.backend.product.model.Product;
+import com.campforest.backend.product.model.ProductImage;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class ProductRegistDto {
+
+	private String productName;
+	private Long productPrice;
+	private String productContent;
+	private String location;
+	private String productType;
+	private String category;
+	private List<String> imageUrls;
+
+
+	public Product toEntity() {
+		return Product.builder()
+			.user_id(1L)
+			.category(category)
+			.productName(productName)
+			.productPrice(productPrice)
+			.productContent(productContent)
+			.location(location)
+			.productType(productType)
+			.interest_hit(0L)
+			.hit(0L)
+			.createdAt(LocalDateTime.now())
+			.updatedAt(LocalDateTime.now())
+			.build();
+	}
+}
