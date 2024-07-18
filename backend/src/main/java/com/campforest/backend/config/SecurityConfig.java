1f7c53080dc0e7d47fbca5466b4702e52a5846b6
LeeJieon <dlwldjs7544@naver.com>
2024-07-18 22:52:52 +0900

    feat: CORS 설정 추가
    - Spring Security 설정 파일에 CORS 설정 추가
    - 추후, setAllowedHeaders 부분에 필요한 Header 만을 설정하도록 변경이 필요
    
    Resolves : #S11P12D208-266
    Related to :

--- backend/src/main/java/com/campforest/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/campforest/backend/config/SecurityConfig.java
@@ -1,20 +1,45 @@
 package com.campforest.backend.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mapping.model.Property;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class SecurityConfig {
 
+	@Value("${cors.allowed-origin}")
+	private String allowedOrigin;
+
+	@Value("${cors.allowed-methods}")
+	private String[] allowedMethods;
+
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(AbstractHttpConfigurer::disable)
+		http.cors(cors -> cors.configurationSource(request -> {
+				CorsConfiguration config = new CorsConfiguration();
+				config.setAllowedOrigins(Collections.singletonList(allowedOrigin));
+				config.setAllowedMethods(Arrays.asList(allowedMethods));
+				config.setAllowCredentials(true);
+				// TODO : 필요한 Header 만을 설정하도록 변경
+				config.setAllowedHeaders(Collections.singletonList("*"));
+				config.setMaxAge(3600L);
+				return config;
+			}))
+			.csrf(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(requests ->
 				requests.requestMatchers("/user/regist/**").permitAll())
 			.formLogin(Customizer.withDefaults())
