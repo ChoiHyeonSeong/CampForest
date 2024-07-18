a6f080e15cce7481eaf1649643b3534cd0ca51f7
이지언 <dlwldjs7544@naver.com>
2024-07-18 17:28:52 +0900

    feat: Spring Security 설정 파일 추가
    - Spring Security 를 사용하기 위한 설정 추가
    
    Resolves :
    Related to : #S11P12D208-264

---
+++ backend/src/main/java/com/campforest/backend/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.campforest.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(requests ->
+				requests.requestMatchers("/user/regist/**").permitAll())
+			.formLogin(Customizer.withDefaults())
+			.httpBasic(Customizer.withDefaults());
+
+		return http.build();
+	}
+}
