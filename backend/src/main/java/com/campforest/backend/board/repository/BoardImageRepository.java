9ed92663740d5ba84d841a5f5053c202d22bdcf8
이준형 <wns0865@naver.com>
2024-07-19 17:15:40 +0900

    feat: 게시글 페이징 기능 추가
    10페이지 단위로 조회, 최신작성글 먼저 조회
    아직 이미지 url 목록은 못불러옴
    
    Resolves:
    Related to: S11P12D208-293

--- backend/src/main/java/com/campforest/backend/board/repository/BoardImageRepository.java
+++ backend/src/main/java/com/campforest/backend/board/repository/BoardImageRepository.java
@@ -1,8 +1,12 @@
 package com.campforest.backend.board.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.campforest.backend.board.entity.BoardImage;
 
 public interface BoardImageRepository extends JpaRepository<BoardImage, Long> {
+
+
 }
