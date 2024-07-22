cdda2494e83c60dbf89e620ae727e9bc0feb1142
이지언 <dlwldjs7544@naver.com>
2024-07-22 17:49:16 +0900

    refactor : ApiResponse가 공통 ErrorCode를 사용하도록 수정
    ApiResponse가 공통 ErrorCode를 사용하도록 수정
    
    Resolves : #S11P12D208-314
    Related to :

---
+++ backend/src/main/java/com/campforest/backend/common/ErrorCode.java
@@ -0,0 +1,30 @@
+package com.campforest.backend.common;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+	// Internal Server Error
+	INTERNAL_SERVER_ERROR("C001", HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다."),
+
+	// User Error
+	USER_REGISTER_FAILED("U001", HttpStatus.BAD_REQUEST, "사용자 등록에 실패했습니다."),
+	USER_NOT_FOUND("U002", HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+	PASSWORD_NOT_MATCH("U003", HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+
+	// Unauthorized
+	AUTHENTICATION_FAILED("A001", HttpStatus.UNAUTHORIZED, "인증에 실패했습니다."),
+	NO_JWT_TOKEN("A002", HttpStatus.UNAUTHORIZED, "JWT 토큰이 없습니다."),
+	INVALID_JWT_TOKEN("A003", HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다."),
+	ACCESS_TOKEN_EXPIRED("A004", HttpStatus.UNAUTHORIZED, "Access Token이 만료되었습니다."),
+	REFRESH_TOKEN_EXPIRED("A005", HttpStatus.UNAUTHORIZED, "Refresh Token이 만료되었습니다."),
+	;
+
+	private final String code;
+	private final HttpStatus httpStatus;
+	private final String message;
+}
