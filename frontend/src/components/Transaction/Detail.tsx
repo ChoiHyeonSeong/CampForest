b0794c675cf7a5c08f53b70c5be64f742e4f78cd
손민락 <dungguk@naver.com>
2024-07-24 15:09:29 +0900

    design: 판매/대여 글쓰기 디자인 수정
    제곧내
    
    Resolves:
    Related to:

--- frontend/src/components/Transaction/Detail.tsx
+++ frontend/src/components/Transaction/Detail.tsx
@@ -6,24 +6,19 @@ import FireGif from '@assets/images/fire.gif';
 function Detail() {
   return (
     <div className="md:mt-[1.5rem] md:mx-auto md:w-[70rem]">
-      <div className="flex h-[25rem] mb-[4rem]">
+      <div className="flex h-[25rem] mb-[2rem]">
         {/* 이미지 */}
         <div className="flex justify-center">
           <div className="size-[25rem] bg-blue-300 rounded-lg"></div>
         </div>
         {/* 내용 */}
-        <div className="px-[2.5rem] w-full">
+        <div className="ps-[2.5rem] w-full">
           <div className="flex text-sm mt-[1rem] mb-[0.5rem] text-[#555555]">
             <div className="me-6">캠핑 장비 {'>'} 텐트</div>
             <div className="text-[#FF7F50] font-bold">대여</div>
             <div className="ms-auto">게시물 신고하기</div>
           </div>
           <div className="text-2xl font-bold">텐트 대여합니다.</div>
-          {/* <div className='flex items-center text-[0.75rem] text-[#555555]'>
-            <div>구미시 인동동</div>
-            <div className='mx-2 text-xs'>|</div>
-            <div>50분 전</div>
-          </div> */}
           <div className="mt-[1.5rem] mb-[2rem] min-h-[11rem] text-sm border-b border-[#EEEEEE] relative">
             <div>주니 텐트 4인용 대여합니다. 일당 15000원 보증금</div>
             <div>없습니다. 문의챗 주세요 @@@@@</div>
@@ -63,21 +58,23 @@ function Detail() {
           </div>
         </div>
       </div>
-      <div className="text-sm mb-[2rem] flex rounded-lg px-[2rem] pt-[2rem] pb-[1rem] bg-[#FAFAFA] shadow-sm">
-        <div className="flex mb-4 w-[25rem]">
-          <div className="w-2/5">
+      <div className="text-sm mb-[2rem] flex rounded-lg ">
+        <div className="flex mb-4 w-[25rem] bg-[#FAFAFA] shadow-sm px-[1rem] py-[1rem] me-[5.5rem]">
+          <div className="w-1/3">
             <div className="text-sm">판매자 정보</div>
             <div className="flex m-2 items-center">
-              <div className="rounded-full size-10 bg-blue-300"></div>
-              <div className="ms-4">
+              {/* <div className="rounded-full size-10 bg-blue-300"></div> */}
+              <div className="">
                 <div>사용자 아이디</div>
                 <div className="text-[#999999]">닉네임</div>
               </div>
             </div>
           </div>
-          <div className="w-3/5">
-            <div>거래 불꽃 온도</div>
-            <div>1273°C</div>
+          <div className="w-2/3 py-[0.5rem]">
+            <div className='flex'>
+              <div>거래 불꽃 온도</div>
+              <div className='ms-auto text-[#FF0000]'>1273°C</div>
+            </div>
             <div className="w-full h-4 bg-gray-200 rounded-full mt-2">
               <div className="h-full rounded-full w-1/2 bg-gradient-to-r from-red-500 to-orange-400 relative">
                 <img
@@ -100,9 +97,9 @@ function Detail() {
       </div>
       <div>
         <div>하치와레미콘님의 거래 상품</div>
-        <div className='flex p-[1rem] space-x-[1rem] justify-center'>
+        <div className='grid grid-cols-5 p-[1rem] gap-[1rem] justify-center'>
           <div className='border rounded-lg'>
-            <div className='bg-red-100 size-[12rem] rounded-lg'></div>
+            <div className='bg-red-100 w-full aspect-1 rounded-lg'></div>
             <div className='p-[0.5rem]'>
               <div>뭔가 감성있는 텐트</div>
               <div className='text-[#FF7F50] text-sm'>대여</div>
@@ -110,7 +107,7 @@ function Detail() {
             </div>
           </div>
           <div className='border rounded-lg'>
-            <div className='bg-red-100 size-[12rem] rounded-lg'></div>
+            <div className='bg-red-100 w-full aspect-1 rounded-lg'></div>
             <div className='p-[0.5rem]'>
               <div>뭔가 감성있는 텐트</div>
               <div className='text-[#FF7F50] text-sm'>대여</div>
@@ -118,7 +115,7 @@ function Detail() {
             </div>
           </div>
           <div className='border rounded-lg'>
-            <div className='bg-red-100 size-[12rem] rounded-lg'></div>
+            <div className='bg-red-100 w-full aspect-1 rounded-lg'></div>
             <div className='p-[0.5rem]'>
               <div>뭔가 감성있는 텐트</div>
               <div className='text-[#FF7F50] text-sm'>대여</div>
@@ -126,7 +123,7 @@ function Detail() {
             </div>
           </div>
           <div className='border rounded-lg'>
-            <div className='bg-red-100 size-[12rem] rounded-lg'></div>
+            <div className='bg-red-100 w-full aspect-1 rounded-lg'></div>
             <div className='p-[0.5rem]'>
               <div>뭔가 감성있는 텐트</div>
               <div className='text-[#FF7F50] text-sm'>대여</div>
@@ -134,7 +131,7 @@ function Detail() {
             </div>
           </div>
           <div className='border rounded-lg'>
-            <div className='bg-red-100 size-[12rem] rounded-lg'></div>
+            <div className='bg-red-100 w-full aspect-1 rounded-lg'></div>
             <div className='p-[0.5rem]'>
               <div>뭔가 감성있는 텐트</div>
               <div className='text-[#FF7F50] text-sm'>대여</div>
