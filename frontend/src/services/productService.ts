a26d3d27e0418b7e56d7117e208cd7e97f96011f
손민락 <dungguk@naver.com>
2024-07-29 15:10:47 +0900

    fix: index.tsx의 <React.StrictMode> 삭제
    Strict 모드는 잠재적인 문제를 잡아내는 모드인데요
    이게 있으면 console.log가 두 번씩 찍히는 문제가 있습니다.
    그래서 주석으로 빼봤습니다.
    
    Resolves:
    Related to:

--- frontend/src/services/productService.ts
+++ frontend/src/services/productService.ts
@@ -1,4 +1,7 @@
+import axios from 'axios';
 import axiosInstance from './authService';
+
+const API_URL = 'http://192.168.100.203:8080';
 
 type ProductRegistDto = {
   productName: string,
@@ -9,6 +12,17 @@ type ProductRegistDto = {
   category: string,
   productImageUrl: {},
   deposit: number | undefined
+}
+
+type SearchParams = {
+  category?: string,
+  productType: string,
+  minPrice?: number,
+  maxPrice?: number,
+  locations?: {},
+  titleKeyword?: string,
+  page?: number,
+  size?: number
 }
 
 export const write = async (productRegistDto: ProductRegistDto) => {
@@ -27,4 +41,9 @@ export const write = async (productRegistDto: ProductRegistDto) => {
     console.error('Product Write failed:', error);
     throw error;
   }
+}
+
+export const list = async (searchParams: SearchParams) => {
+  const response = await axios.get(`${API_URL}/product/search`, { params: searchParams});
+  console.log(response.data.data.content);
 }
