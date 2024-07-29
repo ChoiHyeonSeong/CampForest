4d2369bee1c172f168486e878d7c471d7cb9ed75
최현성 <huns1013@gmail.com>
2024-07-29 17:51:04 +0900

    feat : board axios 구현
    board axios 구현함.
    
    Resolves :
    Related to :

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Recommand from '@components/Board/Recommand';
 import { detail } from '@services/boardService';
-import BoardList from '@components/Board/BoardList';
+import Board from '@components/Board/Board';
+import { getboardlist } from '@services/boardService';
+import { useInView } from 'react-intersection-observer';
 
 function Main() {
+  const [ref, inView] = useInView();
+
+  const [boards, setBoards] = useState<Board[]>([]);
+  const [boardsPage, setboardPage] = useState<number>(0);
+
+  const fetchBoards = async () => {
+    try {
+      const result = await getboardlist(boardsPage, 10);
+      setboardPage(boardsPage+1)
+      console.log(result)
+      // setBoards((prevBoards) => [...prevBoards, result.data.data.content]);
+      setBoards(result.data.data.content);
+    } catch (error) {
+      console.error('게시글 불러오기 실패: ', error);
+    }
+  };
+
+  useEffect(() => {
+    // inView가 true 일때만 실행한다.
+    if (inView) {
+      console.log(inView, '무한 스크롤 요청')
+      fetchBoards()
+      console.log(boards, boardsPage)
+    }
+  }, [inView]);
+
   const getDetail = () => {
     detail(1);
   }
   
   return (
-    <div className='flex justify-center'>
-      <div className='hidden lg:block w-[15rem]'/>
-      <div className='w-full md:w-[40rem]'>
-        <div onClick={getDetail} className='cursor-pointer'>1번 게시글 자세히 보기</div>
-        <BoardList />
+    <div>
+      <div className='flex justify-center'>
+        <div className='hidden lg:block w-[15rem]'/>
+        <div className='w-full md:w-[40rem]'>
+          <div onClick={getDetail} className='cursor-pointer'>1번 게시글 자세히 보기</div>
+          {boards?.map((board) => (
+            <Board key={board.boardId} board={board} />
+          ))}
+        </div>
+        <div>
+          <Recommand />
+        </div>
       </div>
-      <div>
-        <Recommand />
-      </div>
-    </div>
+
+      {/* intersection observer */}
+      <div ref={ref}></div>
+    </div> 
   )
 }
 
