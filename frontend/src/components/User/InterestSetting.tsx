121d171345d42390764ec941a0a34e1b2ffb53b3
조소연 <lilyjo128@naver.com>
2024-08-01 16:14:59 +0900

    Fix : User 컴포넌트 내 css 클래스명 변경
    Resloves:
    Related to:

--- frontend/src/components/User/InterestSetting.tsx
+++ frontend/src/components/User/InterestSetting.tsx
@@ -6,6 +6,7 @@ type Props = {
   saveFunction: (params: object) => void;
 }
 
+// 관심 태그 설정
 type InterestTag = '선호 캠핑 분위기' | '산' | '해변' | '숲' | '도심' | '근교' | '계곡' | '섬' |
                    '선호 캠핑 지역' | '수도권' | '강원도' | '충청도' | '전라도' | '경상도' | '제주도' |
                    '선호 캠핑 동반자' | '가족' | '연인' | '혼자' | '친구' | '반려동물' |
@@ -64,37 +65,142 @@ const InterestSetting = (props: Props) => {
     }
   }, [showWarning]);
 
+  
   return (
-    <div className="mb-6 flex flex-col items-center">
-      <label className="w-full block text-gray-70 text-left font-medium text-lg">
-        <p className='text-left'>관심사 설정<span className='text-red-500 text-md'> *</span></p>
+    <div className="flex flex-col items-center mb-[1.5rem]">
+      <label
+        className={`
+          block w-full
+          text-light-text
+          dark:text-dark-text
+          text-left font-medium text-[1.125rem]
+        `}
+      >
+        <p className='text-left'>관심사 설정 
+          <span
+            className={`
+              ms-[0.25rem]
+              text-light-heart
+              dark:text-dark-heart
+            `}
+            >
+              *
+          </span>
+        </p>
       </label>
-      <div className='w-full text-left font-light mt-1 mb-4'>
-        <p className=''>취향에 맞는 컨텐츠를 추천해드립니다!</p>
-        <p>원하는 유형의 캠핑태그를 <span className='font-medium'>6개</span> 선택해주세요.</p>
+
+      <div
+        className={`
+          w-full mt-1 mb-4
+          text-light-text
+          dark:text-dark-text
+          text-left font-light
+        `}
+      >
+        <p>취향에 맞는 컨텐츠를 추천해드립니다!</p>
+        <p>원하는 유형의 캠핑태그를 
+          <span 
+            className={`
+              mx-[0.25rem]
+              font-medium
+            `}
+          >
+          6개
+          </span>
+          선택해주세요.
+        </p>
       </div>
 
-      <div className='w-full h-fit py-3 px-3 shadow-lg mt-2 rounded-xl bg-slate-50'>
-        <div className='mb-4'>
-          <p className='font-medium text-lg'>6개 중 <span className='text-[#ff7f50] text-2xl'>{selectedCount}</span> 개 선택됨</p>
-          <div className={`text-sm text-red-500 mt-2 ${showWarning ? 'block animate-shake' : 'hidden'}`}>캠핑 태그는 6개만 선택 가능합니다.</div>
+      <div 
+        className={`
+          w-full h-fit mt-[0.5rem] p-[0.75rem] 
+          bg-slate-50
+          dark:bg-dark-white
+          shadow-lg rounded-xl
+        `}
+      >
+        <div className='mb-[1rem]'>
+          <p 
+            className={`
+              text-light-text
+              dark:text-dark-text
+              font-medium text-[1.125rem] 
+            `}
+          >
+            6개 중
+            <span 
+              className={`
+                mx-[0.25rem]
+                text-light-signature
+                dark:text-dark-signature
+                text-[1.5rem]
+              `}
+            >
+              {selectedCount}
+            </span>
+            개 선택됨
+          </p>
+          <div
+            className={`
+              mt-[0.5rem]
+              text-light-heart
+              dark:text-dark-heart
+              text-[0.8758rem]
+            ${showWarning ? 'block animate-shake' : 'hidden'}
+            `}
+          >
+            캠핑 태그는 6개만 선택 가능합니다.
+          </div>
         </div>
 
         {Object.entries(interestTagList).map(([category, tags]) => (
-          <div className='interesting-cate mb-6' key={category}>
-            <div className='font-medium text-gray-500 mb-3'>{`# ${category}`}</div>
-            <div className="flex flex-wrap gap-2 mt-1">
+          <div className='mb-[1.5rem]' key={category}>
+            {/* 카테고리별 주제 */}
+            <div
+              className={`
+                mb-[0.75rem] 
+                text-light-text-secondary 
+                dark:text-dark-text-secondary 
+                font-medium
+              `}
+            >
+              {`# ${category}`}
+            </div>
+
+            {/* 카테고리별 태그 */}
+            <div className="flex flex-wrap gap-[0.5rem] mt-[0.25rem]">
               {tags.map(tag => (
                 <span
                   key={tag}
-                  className={`duration-200 ps-3 py-1 border-[1.5px] rounded-full flex items-center cursor-pointer pr-4 ${
-                    interestChecking[tag as InterestTag] 
-                      ? 'border-[#ff7f50] text-[#ff7f50]'
-                      : 'border-gray-300 text-gray-600 hover:border-[#ff7f50]'
-                  }`}
+                  className={`
+                    flex items-center ps-[0.75rem] py-[0.25rem] pr-[1rem]
+                    duration-200 border-[1.5px] rounded-full cursor-pointer
+
+                    ${interestChecking[tag as InterestTag] ?
+                      `border-light-signature text-light-signature
+                        dark:border-dark-signature dark:text-dark-signature` :
+                      `border-light-border-1 text-light-text-secondary hover:border-light-signature
+                      dark:border-dark-border-1 dark:text-dark-text-secondary hover:dark:border-dark-signature`
+                    }
+                  `}
                   onClick={() => handleTagClick(tag)}
                 >
-                  {interestChecking[tag as InterestTag] ? <CheckIcon className='mr-1 size-4 lg:size-5' /> : <PlusIcon className='mr-1 size-4 lg:size-5 ' />}
+                  {interestChecking[tag as InterestTag] ?
+                    <CheckIcon
+                      className={`
+                        size-[1rem] lg:size-[1.25rem] mr-[0.25rem]
+                        fill-light-border-icon
+                        dark:fill-dark-border-icon
+                      `}
+                    /> :
+                    <PlusIcon 
+                      className={`
+                        size-[1rem] lg:size-[1.25rem] mr-[0.25rem]
+                        stroke-light-border-icon
+                        dark:stroke-dark-border-icon
+                      `}
+                    />
+                  }
                   {tag}
                 </span>
               ))}
--- frontend/src/components/User/PasswordChange.tsx
+++ frontend/src/components/User/PasswordChange.tsx
@@ -3,30 +3,114 @@ import { ReactComponent as LeftArrow } from '@assets/icons/arrow-left.svg'
 
 const PasswordChange = () => {
   return (
-    <div className='md:mt-[10rem] mx-auto p-6 md:max-w-3xl lg:w-[40rem] lg:p-0'>
-      <div className='max-md:flex items-center md:text-center pb-[0.75rem] text-[1.5rem] md:text-[2rem]  border-black mb-[3rem]'>
-        <LeftArrow className='me-[1rem] md:hidden' />
+    <div className='lg:w-[40rem] md:max-w-[48rem] mx-auto md:mt-[10rem] p-[1.5rem] lg:p-0'>
+      <div
+        className={`
+          max-md:flex items-center pb-[0.75rem] mb-[3rem]
+          md:text-center text-[1.5rem] md:text-[2rem]
+        `}
+      >
+        <LeftArrow
+          className={`
+            md:hidden me-[1rem]
+            fill-light-border-icon
+            dark:fill-dark-border-icon
+          `}
+        />
         <div>비밀번호 변경</div>
       </div>
-      <div className='mx-auto w-[25rem] mt-[25%] md:mt-[5rem] px-[1rem]'>
-        <div className='flex text-sm text-[#777777] mb-[1rem]'>
+
+      {/* 비밀번호재설정 큰 부모 */}
+      <div className='w-[25rem] mx-auto mt-[25%] md:mt-[5rem] px-[1rem]'>
+        <div
+          className={`
+            flex mb-[1rem]
+            text-light-text 
+            dark:text-dark-text
+            text-sm
+          `}
+        >
           <div className='font-semibold'>ssafy123@test.com</div>
           <div>의 비밀번호를 변경합니다.</div>
         </div>
         <div className='text-center'>
-          <input type='password' placeholder='새 비밀번호' className='text-sm border border-black py-[0.75rem] px-[1rem] rounded-md w-full max-w-[23rem] focus:outline-none mb-[1rem]'/>
-          <input type='password' placeholder='비밀번호 확인' className='text-sm border border-black py-[0.75rem] px-[1rem] rounded-md w-full max-w-[23rem] focus:outline-none mb-[1rem]'/>
-          <div className='text-sm text-[#777777] font-semibold space-y-[1rem] ps-[1rem]'>
-            <div className='flex items-center space-x-[0.5rem]'>
-              <div className='size-[0.5rem] rounded-full bg-red-500'></div>
-              <div>최소 8자리 이상, 최대 16자리 이하</div>
+          <input
+            type='password'
+            placeholder='새 비밀번호'
+            className={`
+               w-full max-w-[23rem] border mb-[1rem] px-[1rem] py-[0.75rem]
+              border-light-border-3 
+              dark:border-dark-border-3
+              rounded-md focus:outline-none text-sm
+            `}
+          />
+           <input
+            type='password'
+            placeholder='비밀번호 확인'
+            className={`
+               w-full max-w-[23rem] border mb-[1rem] px-[1rem] py-[0.75rem]
+              border-light-border-3 
+              dark:border-dark-border-3
+              rounded-md focus:outline-none text-sm
+            `}
+          />
+
+          <div
+            className={`
+              space-y-[1rem]
+              text-light-text-secondary
+              dark:text-dark-text-secondary
+              text-sm font-semibold
+            `}
+          >
+            <div className='flex items-center'>
+              <div
+                className={`
+                  size-[0.5rem] me-[0.5rem]
+                  bg-light-heart
+                  dark:bg-dark-heart
+                  rounded-full
+                `}
+              />
+              <div
+                className={`
+                  text-light-text-secondary 
+                  dark:text-dark-text-secondary
+                `}
+              >
+                최소 8자리 이상, 최대 16자리 이하
+              </div>
             </div>
-            <div className='flex items-center space-x-[0.5rem]'>
-              <div className='size-[0.5rem] rounded-full bg-red-500'></div>
-              <div>영대소문자, 숫자, 특수문자 중 2종류 이상 포함</div>
-            </div>
+            <div className='flex items-center'>
+              <div
+                className={`
+                  size-[0.5rem] me-[0.5rem]
+                  bg-light-heart
+                  dark:bg-dark-heart
+                  rounded-full
+                `}
+              />
+              <div
+                className={`
+                  text-light-text-secondary 
+                  dark:text-dark-text-secondary
+                `}
+              >
+                영대소문자, 숫자, 특수문자 중 2종류 이상 포함
+              </div>
+            </div>            
           </div>
-          <div className='mx-auto bg-black text-sm text-white py-[0.75rem] max-w-[23rem] mt-[3.5rem] cursor-pointer'>비밀번호 변경</div>
+          <div
+            className={`
+              w-full max-w-[23rem] mt-[3.25rem] mx-auto py-[0.75rem] 
+              bg-light-signature text-light-text-white
+              dark:bg-dark-signature dark:text-dark-text-white
+              text-sm md:text-base cursor-pointer
+            
+            `}
+          >
+            비밀번호 변경
+          </div>
         </div>
       </div>
     </div>
