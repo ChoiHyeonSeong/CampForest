64cb4222db8fe60db1d2cb728cfb53e797fb607e
이지언 <dlwldjs7544@naver.com>
2024-08-07 16:33:17 +0900

    fix : 잘못된 API 요청에 대한 Filter 추가
    잘못된 API 요청에 대한 Filter 추가
    
    Resolves :
    Related to :

--- backend/src/main/java/com/campforest/backend/common/ErrorCode.java
+++ backend/src/main/java/com/campforest/backend/common/ErrorCode.java
@@ -11,6 +11,8 @@ public enum ErrorCode {
 	// Internal Server Error
 	INTERNAL_SERVER_ERROR("C001", HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다."),
 	NOT_FOUND_API_URL("C002", HttpStatus.NOT_FOUND, "요청한 API url을 찾을 수 없습니다."),
+	RESOURCE_NOT_FOUND("C003", HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다."),
+	ACCESS_DENIED("C004", HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
 
 	// User Error
 	USER_REGISTER_FAILED("U001", HttpStatus.BAD_REQUEST, "사용자 등록에 실패했습니다."),
