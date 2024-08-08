1b48e338549368d202ac96da40422dc642f7ffd7
조소연 <lilyjo128@naver.com>
2024-08-08 17:07:00 +0900

    Feat : 상단 알림창 제작
    UI 구현 (다크모드)
    
    Resolves:
    Related to:#

---
+++ frontend/src/components/Navbar/NavTopPushModal.tsx
@@ -0,0 +1,70 @@
+
+import React, { useEffect }  from 'react'
+import { ReactComponent as CloseIcon } from '@assets/icons/close.svg'
+import Notification from '@components/Notification/Notification';
+import { logout } from '@services/authService';
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+
+const NavTopPushModal = (props: Props) => {
+
+  const loggedout = async () => {
+    await logout()
+    window.location.reload();
+  }
+  useEffect(() => {
+    window.addEventListener('resize', props.onClose);
+  }, []);
+
+
+
+  return (
+    <div 
+      className={`
+        ${props.isOpen ? 'block' : 'hidden'}
+        absolute right-0 top-[3rem] z-[100] w-[20rem] max-h-[40rem] p-[0.75rem]
+        bg-light-bgbasic
+        dark:bg-dark-bgbasic
+        rounded-lg shadow-lg overflow-hidden
+      `}
+    >
+      {/* 상단 */}
+      <div className='flex justify-between'>
+        <p
+          className='
+            mb-[0.75rem]
+            
+            text-xl font-semibold
+          '
+        >
+          알림
+        </p>
+        <CloseIcon
+          onClick={props.onClose}
+          className='
+            ize-[1.5rem]
+            fill-light-border-icon
+            dark:fill-dark-border-icon
+            cursor-pointer
+          '
+        />
+      </div>
+
+      <div className='rounded overflow-hidden'>
+        <Notification />
+        <Notification />
+        <Notification />
+        <Notification />
+        <Notification />
+      </div>
+      
+
+    </div>
+  )
+}
+
+export default NavTopPushModal
--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,84 +1,88 @@
-import React, { useEffect, useState } from 'react';
-// import ReactFullpage from '';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useRef  } from 'react'
+import { Link } from 'react-router-dom'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 import LogoWhite from '@assets/logo/logo-darkmode.svg'
 
-type Props = {}
+const LandingPage = () => {
+  const [currentPage, setCurrentPage] = useState(0)
+  const [direction, setDirection] = useState('down')
+  const textRef = useRef(null)
 
-const LandingPage = (props: Props) => {
-  const [currentSlide, setCurrentSlide] = useState(0);
   const texts = [
-    '잠자던 캠핑용품,새로운 시작이 되다',
-    '캠핑장비로 아직도 고민중이신가요?',
-    '집에만 두기 아까운 장비를 빌려주고 수익을 만들어보세요 !',
-    '캠핑동선에 맞게 원하는 장소에서장비를 빌리고 반납하세요!'
-  ];
+    '잠자던 캠핑용품,<br/>새로운 시작이 되다',
+    '캠핑장비로 아직도<br/>고민중이신가요?',
+    '집에만 두기 아까운 장비를<br/>빌려주고 수익을 만들어보세요!',
+    '캠핑동선에 맞게 원하는 장소에서<br/>장비를 빌리고 반납하세요!'
+  ]
 
   useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: false,
-    });
-  }, []);
+    const handleScroll = (e: { deltaY: number }) => {
+      if (e.deltaY > 0 && currentPage < texts.length - 1) {
+        setCurrentPage(prev => prev + 1)
+        setDirection('down')
+      } else if (e.deltaY < 0 && currentPage > 0) {
+        setCurrentPage(prev => prev - 1)
+        setDirection('up')
+      }
+    }
+
+    window.addEventListener('wheel', handleScroll)
+
+    return () => window.removeEventListener('wheel', handleScroll)
+  }, [currentPage, texts.length])
+
 
   return (
-    <></>
-  //   <ReactFullpage
-  //   scrollingSpeed={1000}
-  //   onLeave={(origin, destination) => {
-  //     setCurrentSlide(destination.index);
-  //   }}
-  //   render={({ state, fullpageApi }) => (
-  //     <ReactFullpage.Wrapper>
-  //       {texts.map((text, index) => (
-  //         <div key={index} className="section">
-  //           <div className='fixed flex justify-center pt-[15%] inset-0 w-screen h-screen z-[100] bg-landing-bg-lg bg-cover bg-center bg-no-repeat'>
-  //             <div className='flex flex-col justify-between items-center h-[45%] text-white text-center'>
-  //               <div className=''>
-  //                 <img
-  //                   alt='로고' 
-  //                   src={LogoWhite}
-  //                   className='w-[14rem]'  
-  //                 />
-  //               </div>
-  //               <div 
-  //                 className='text-[3.2rem] font-bold'
-  //                 data-aos="fade-up"
-  //                 data-aos-duration="1000"
-  //               >
-  //                 <p>{text}</p>
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       ))}
+    <div className='fixed inset-0 w-screen h-screen z-[100] pt-[10%] bg-landing-bg-lg bg-cover bg-center bg-no-repeat overflow-hidden'>
+      <div className='flex flex-col justify-between items-center h-[50%] text-white text-center'>
+        <div>
+          <img
+            alt='로고' 
+            src={LogoWhite}
+            className='w-[14rem]'  
+          />
+        </div>
+        <div className='h-[200px] overflow-hidden'> {/* 텍스트 컨테이너 높이 지정 */}
+          {texts.map((text, index) => (
+            <div
+              key={index}
+              className='text-[3.2rem] font-bold transition-transform duration-500 absolute left-0 right-0'
+              style={{
+                transform: `translateY(${(index - currentPage) * 100}%)`,
+                opacity: index === currentPage ? 1 : 0,
+                transition: 'transform 1.5s ease, opacity 0.3s ease',
+              }}
+              dangerouslySetInnerHTML={{ __html: text }}
+            />
+          ))}
+        </div>
+      </div>
 
-  //       {/* 페이지네이션 */}
-  //       <div className="fixed right-[2%] top-1/2 -translate-y-1/2 text-white">
-  //         {texts.map((_, index) => (
-  //           <div
-  //             key={index}
-  //             className={`w-2 h-2 rounded-full my-2 cursor-pointer ${
-  //               currentSlide === index ? 'bg-white' : 'bg-gray-500'
-  //             }`}
-  //             onClick={() => fullpageApi.moveTo(index + 1)}
-  //           />
-  //         ))}
-  //       </div>
+      {/* 페이지네이션 */}
+      <div className="fixed right-[2%] top-1/2 transform -translate-y-1/2 text-white">
+        {texts.map((_, index) => (
+          <div 
+            key={index}
+            className={`w-2 h-2 my-2 rounded-full cursor-pointer ${currentPage === index ? 'bg-white' : 'bg-gray-500'}`}
+            onClick={() => {
+              setDirection(index > currentPage ? 'down' : 'up')
+              setCurrentPage(index)
+            }}
+          />
+        ))}
+      </div>
 
-  //       {/* 스크롤 표시 */}
-  //       <div className='fixed bottom-[5%] left-1/2 -translate-x-1/2 text-white'>SCROLL</div>
+      {/* 스크롤 표시 */}
+      <div className='fixed bottom-[5%] left-1/2 -translate-x-1/2 text-white'>SCROLL</div>
 
-  //       {/* 스킵버튼 */}
-  //       <Link to="/" className='fixed right-[5%] bottom-[5%] bg-light-gray-1 px-[1rem] py-[0.25rem] rounded text-white'>
-  //         SKIP
-  //       </Link>
-  //     </ReactFullpage.Wrapper>
-  //   )}
-  // />
-);
-};
+      {/* 스킵버튼 */}
+      <Link to="/" className='fixed right-[5%] bottom-[5%] bg-light-gray-1 px-[1rem] py-[0.25rem] rounded text-black'>
+        SKIP
+      </Link>
+    </div>
+  )
+}
 
 export default LandingPage
