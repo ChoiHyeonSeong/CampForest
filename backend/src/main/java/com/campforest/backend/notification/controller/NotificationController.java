2f5775e782970a0d0b0ac89ea69b74e52e634881
이지언 <dlwldjs7544@naver.com>
2024-08-09 15:47:06 +0900

    fix : 알림 Controller 수정
    알림 Controller 수정

--- backend/src/main/java/com/campforest/backend/notification/controller/NotificationController.java
+++ backend/src/main/java/com/campforest/backend/notification/controller/NotificationController.java
@@ -21,6 +21,7 @@ import com.campforest.backend.notification.service.SseEmitters;
 import com.campforest.backend.user.model.Users;
 import com.campforest.backend.user.service.UserService;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -33,11 +34,14 @@ public class NotificationController {
 	private final SseEmitters sseEmitters;
 
 	@GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-	public SseEmitter subscribe(@AuthenticationPrincipal UserDetails userDetails) {
+	public SseEmitter subscribe(@AuthenticationPrincipal UserDetails userDetails, HttpServletResponse response) {
 		Users users = userService.findByEmail(userDetails.getUsername())
 			.orElseThrow(() -> new UsernameNotFoundException(ErrorCode.USER_NOT_FOUND.getMessage()));
 
 		SseEmitter emitter = sseEmitters.createEmitter(users.getUserId());
+		response.setHeader("X-Accel-Buffering", "no");
+		response.setHeader("Cache-Control", "no-cache");
+		response.setHeader("Connection", "keep-alive");
 
 		try {
 			emitter.send(SseEmitter.event()
