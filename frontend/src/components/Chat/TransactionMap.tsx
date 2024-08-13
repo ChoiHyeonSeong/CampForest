d89b2f38bc31433850562a3c11ca0be4373f72e9
손민락 <dungguk@naver.com>
2024-08-13 21:54:16 +0900

    feat:

--- frontend/src/components/Chat/TransactionMap.tsx
+++ frontend/src/components/Chat/TransactionMap.tsx
@@ -7,6 +7,7 @@ type Props = {
 
 let mapInstance: naver.maps.Map | null = null;
 let infoWindow: naver.maps.InfoWindow | null = null;
+let marker: naver.maps.Marker | null = null;
 let address = '';
 
 const loadScript = (src: string, callback: () => void) => {
@@ -24,12 +25,7 @@ type MapInformationProps = {
 };
 
 const MapInformation: React.FC<MapInformationProps> = ({ options, latitude, longitude }) => {
-  const [location, setLocation] = useState<{ latitude: number; longitude: number } | null>(null);
-
-  const handleSuccess = (position: GeolocationPosition) => {
-    const { latitude, longitude } = position.coords;
-    setLocation({ latitude, longitude });
-  };
+  const [isStart, setIsStart] = useState(false);
 
   const initMap = () => {
     const mapOptions = {
@@ -39,8 +35,8 @@ const MapInformation: React.FC<MapInformationProps> = ({ options, latitude, long
         position: naver.maps.Position.TOP_RIGHT,
       },
       center: new naver.maps.LatLng(
-        location ? location.latitude : latitude,
-        location ? location.longitude : longitude,
+        latitude,
+        longitude,
       ),
       zoom: 15,
     };
@@ -48,60 +44,24 @@ const MapInformation: React.FC<MapInformationProps> = ({ options, latitude, long
     if (document.getElementById('map')) {
       mapInstance = new naver.maps.Map('map', mapOptions);
 
-      const marker = new naver.maps.Marker({
+      marker = new naver.maps.Marker({
         position: new naver.maps.LatLng(latitude, longitude),
         map: mapInstance,
       });
 
       // 클릭한 곳으로 마커랑 인포 윈도우 이동
       naver.maps.Event.addListener(mapInstance, 'click', (event) => {
-        mapInstance?.setCenter(new naver.maps.LatLng(event.coord.y, event.coord.x));
-        marker.setPosition(new naver.maps.LatLng(event.coord.y, event.coord.x));
-        let contentString = `<div style='padding:10px;'>안녕하세요</div>`;
-
-        naver.maps.Service.reverseGeocode(
-          {
-            coords: event.coord,
-            orders: [
-              naver.maps.Service.OrderType.LEGAL_CODE,
-              naver.maps.Service.OrderType.ADDR,
-            ].join(','),
-          },
-          function (status, response) {
-            if (status === naver.maps.Service.Status.ERROR) {
-              return alert('Something Wrong');
-            }
-
-            if(response.v2.results[0]) {
-              address = `${response.v2.address.jibunAddress}`;
-            } 
-            else {
-              address = `위치 정보가 없습니다.`
-            }
-
-            contentString = `<div style='padding:10px;'><div>${address}</div></div>`;
-            
-            infoWindow = new naver.maps.InfoWindow({
-              content: contentString,
-              disableAnchor: true,
-              pixelOffset: new naver.maps.Point(0, -10),
-            });
-            if (mapInstance) {
-              infoWindow?.open(mapInstance, marker);
-            }
-          },
-        );
+        
       });
     }
   };
+  const handleStart = () => {
+    setTimeout(() => {
+      setIsStart(true);
+    }, 300);
+  } 
 
   useEffect(() => {
-    const { geolocation } = navigator;
-
-    if (geolocation) {
-      geolocation.getCurrentPosition(handleSuccess, null, options);
-    }
-
     if (typeof naver === 'undefined' || !naver.maps) {
       loadScript(
         'https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=52k7jcq0yh&callback=initMap&submodules=geocoder',
@@ -110,13 +70,49 @@ const MapInformation: React.FC<MapInformationProps> = ({ options, latitude, long
     } else {
       initMap();
     }
+    handleStart();
   }, []);
 
   useEffect(() => {
-    if (location && mapInstance) {
-      mapInstance.setCenter(new naver.maps.LatLng(location.latitude, location.longitude));
+    if(isStart && marker) {
+      mapInstance?.setCenter(new naver.maps.LatLng(latitude, longitude));
+      marker.setPosition(new naver.maps.LatLng(latitude, longitude));
+      let contentString = `<div style='padding:10px;'>안녕하세요</div>`;
+
+      naver.maps.Service.reverseGeocode(
+        {
+          coords: new naver.maps.LatLng(latitude, longitude),
+          orders: [
+            naver.maps.Service.OrderType.LEGAL_CODE,
+            naver.maps.Service.OrderType.ADDR,
+          ].join(','),
+        },
+        function (status, response) {
+          if (status === naver.maps.Service.Status.ERROR) {
+            return alert('Something Wrong');
+          }
+
+          if(response.v2.results[0]) {
+            address = `${response.v2.address.jibunAddress}`;
+          } 
+          else {
+            address = `위치 정보가 없습니다.`
+          }
+
+          contentString = `<div style='padding:10px;'><div>${address}</div></div>`;
+          
+          infoWindow = new naver.maps.InfoWindow({
+            content: contentString,
+            disableAnchor: true,
+            pixelOffset: new naver.maps.Point(0, -10),
+          });
+          if (mapInstance && marker) {
+            infoWindow?.open(mapInstance, marker);
+          }
+        },
+      );
     }
-  }, [location]);
+  }, [isStart])
 
   return (
     <div className={`w-full h-2/3`}>
@@ -138,8 +134,8 @@ const TransactionMap: React.FC<Props> = ({ latitude, longitude }) => {
       <div className={`w-full h-full`}>
         <MapInformation
           options={geolocationOptions}
-          latitude={37.5666805}
-          longitude={126.9784147}
+          latitude={latitude}
+          longitude={longitude}
         />
       </div>
     </div>
