4880f218f76248aeb04fcbe963b085342fb59fa5
최현성 <huns1013@gmail.com>
2024-08-16 04:24:27 +0900

    last Change

--- frontend/src/components/Board/BoardModify.tsx
+++ frontend/src/components/Board/BoardModify.tsx
@@ -179,6 +179,23 @@ const BoardModify = (props: Props) => {
     }
   }, [title, content])
 
+  // 안드로이드 뒤로가기 버튼 이벤트 핸들러
+  const handleBackButton = (event: PopStateEvent) => {
+    event.preventDefault();
+    props.modifyClose()
+  };
+
+  useEffect(() => {
+    // 컴포넌트 마운트 시 이벤트 리스너 추가
+    window.history.pushState(null, '', window.location.href);
+    window.addEventListener('popstate', handleBackButton);
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('popstate', handleBackButton);
+    };
+  }, []);
+
   return (
     <div 
       className={`
